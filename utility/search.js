c5662b65702921404d65261cce6360ebf538d165
s094392 <s094392@gmail.com>
2017-11-22 04:11:16 +0800

    done with return bug

--- utility/search.js
+++ utility/search.js
@@ -5,18 +5,18 @@ var search = function(address, data){
     geocoder.geocode(address, function(err, res){
         if(err) console.log(err);
         else{
-            console.log(res);
             var lat = parseFloat(res[0].latitude);
             var lon = parseFloat(res[0].longitude);
             for(var i of data){
-                var tmp = (Math.pow(lat-i.lat, 2) + Math.pow(lon-i.lon, 2));
+                var tmp = (Math.pow(lat-i.lat, 2) + Math.pow(lon-i.lng, 2));
                 if(tmp<min){
                     min = tmp;
                     fdata = i.data;
-                    console.log("fdata", fdata);
                 }
             }
         }
-    })
+        console.log("最近的", fdata);
+    });
+    return fdata;
 }
 module.exports = search;
