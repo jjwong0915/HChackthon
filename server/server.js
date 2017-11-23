4bc0455b80a3e336ecfb65c342b24c6ed8cdd907
s094392 <s094392@gmail.com>
2017-11-23 13:54:43 +0800

    fix search.js and build search page

--- server/server.js
+++ server/server.js
@@ -5,15 +5,29 @@ let express = require('express');
 let cons = require('consolidate');
 
 let app = express();
+let bodyParser = require('body-parser');
+let search = require('../utility/search.js');
+var med = require('../data/medical/data.json');
 
 app.set('views', path.join(__dirname, '../client/template'));
 app.set('view engine', 'html');
 app.engine('html', cons.hogan);
 app.use(express.static(path.join(__dirname, '../client/public')));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
 	res.render('index', {title: 'HC hackthon'});
 });
+
+app.post('/', (req, res) => {
+    var address = req.body.address;
+    var tmp = search(address, med, 0.01, function(result){
+    	res.render('result', {mednum: result.length, med: result});
+    });
+});
+
 
 app.listen(3000, () => {
 	console.log('Server has been started!');
--- utility/search.js
+++ utility/search.js
@@ -6,22 +6,21 @@ const disCalc = (latA, lngA, latB, lngB) => (
 
 // search(address:String, data: [{lat:Number, lng:Number}], d:Number):
 // calculate how many items are around the address in the distance(degrees)
-const search = function(address, data, distance) {
-    return geocoder.geocode(address).then((res) => {
-    	if(res.length < 1) {
-    		throw new Error("Geocoding API has no result for the address.");
-    	}
+const search = function(address, data, distance, callback) {
+    var result = [];
+    geocoder.geocode(address, function(err, res){
+        if(res.length < 1) {
+            throw new Error("Geocoding API has no result for the address.");
+        }
 
         const lat = res[0].latitude;
         const lng = res[0].longitude;
-
-        var counter = 0;
         for(const item of data) {
-        	if(disCalc(lat, lng, item.lat, item.lng) < distance) {
-        		counter++;
-        	}
+            if(disCalc(lat, lng, item.lat, item.lng) < distance) {
+                result.push(item.data);
+            }
         }
-        return counter;
+        callback(result)
     });
 };
 
