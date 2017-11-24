1975365c625d9562b12fdbe04d226151e3cb2f57
JJ Wong <jjwong0915@gmail.com>
2017-11-24 21:20:30 +0800

    Change search utility with promise API

--- utility/search.js
+++ utility/search.js
@@ -7,20 +7,22 @@ const disCalc = (latA, lngA, latB, lngB) => (
 // search(address:String, data: [{lat:Number, lng:Number}], d:Number):
 // calculate how many items are around the address in the distance(degrees)
 const search = function(address, data, distance, callback) {
-    var result = [];
-    geocoder.geocode(address, function(err, res){
+    return geocoder.geocode(address).then((res) => {
         if(res.length < 1) {
             throw new Error("Geocoding API has no result for the address.");
         }
 
         const lat = res[0].latitude;
         const lng = res[0].longitude;
+        
+        const result = [];
         for(const item of data) {
             if(disCalc(lat, lng, item.lat, item.lng) < distance) {
                 result.push(item.data);
             }
         }
-        callback(result)
+
+        return result;
     });
 };
 
