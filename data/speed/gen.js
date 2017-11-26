89de20156b866c6b92feb00909e21685a70acff7
s094392 <s094392@gmail.com>
2017-11-26 11:02:04 +0800

    add speed data

---
+++ data/speed/gen.js
@@ -0,0 +1,15 @@
+let speed = require('./speed.json');
+let fs = require('fs');
+data = [];
+for (const item of speed) {
+    data.push({lat: item["經度"], lng: item["緯度"], data: {position: item["地 點"], limit: item["速限"]}});
+}
+
+
+fs.writeFile("./data.json", JSON.stringify(data), (err) => {
+    if (err) {
+        console.error(err);
+        return;
+    };
+    console.log("File has been created");
+});
