3cc4ea3408416a0054e473c6399898889478c011
s094392 <s094392@gmail.com>
2017-11-26 14:38:53 +0800

    fix data

--- data/refuge/gen.js
+++ data/refuge/gen.js
@@ -3,7 +3,7 @@ let fs = require('fs');
 
 var data = [];
 for (const item of refuge) {
-    if(item["緯度"]=""||item["經度"]=="") continue;
+    if(item["緯度"]==""||item["經度"]=="") continue;
     data.push({lat: parseFloat(item["緯度"]), lng: parseFloat(item["經度"]), data:{name: item["收容場所名稱"], peopleNumber: item["預估收容人數"], phone: item["聯絡電話"], cellphone: item["手機"], admin: item["管理人"]}});
 }
 
